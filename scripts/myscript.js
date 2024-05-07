a6bf5bfa09fd03649cc7e8b68da34bd5f10a6183
RemiDelobelle <r0903204@student.vives.be>
2024-05-07 14:35:15 +0200

    Removed unnecessary comments

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -63,7 +63,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 });
             }
 
-
             // Als beeld gevraagd wordt...
             if(deviceId != "None")
             {
@@ -95,7 +94,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 
-    // Button to trigger text extraction
     const btnExtract = document.getElementById("btnExtract");
     const btnSelectFile = document.getElementById("btnSelectFile");
     const btnSave = document.getElementById("btnSave");
@@ -111,7 +109,6 @@ document.addEventListener('DOMContentLoaded', function() {
         checkSelectValue();        
     });
 
-    // Event listener for the "Extract Text" button
     btnExtract.addEventListener("click", function() {
         if (photoElement.innerHTML === '' && selectMediaDevice.value === 'None') {
             console.error("No photo found to extract text.");
@@ -121,41 +118,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // checkPrevDBItem();
 
-        // Show loading animation
         loadingAnimation.style.display = 'block';
 
         // Check and remove the previous DB item if its text is empty
         checkPrevDBItem().then(() => {
             if (selectMediaDevice.value === 'None') {
-                // If videoElement is visible, run the normal extraction process
                 SavePhotoFromFile().then(() => {
-                    // Hide loading animation when text is displayed
                     loadingAnimation.style.display = 'none';
                 });
             } else {
-                // If videoElement is not visible, extract text from the selected image
                 takeAndSavePhoto().then(() => {
-                    // Hide loading animation when text is displayed
                     loadingAnimation.style.display = 'none';
                 });
             }
         });
     });
 
-    // Event listener for the "Choose file" button
     btnSelectFile.addEventListener("click", function() {
-        // Closes camera stream
         if (_mediaStream) {
             _mediaStream.getTracks().forEach(track => {
                 track.stop();
             });
         }
-        // Shut off the camera by hiding the video element
         if (videoElement) {
             videoElement.style.display = 'none';
         }
 
-        // Set the selectMediaDevice to "None"
         if (selectMediaDevice) {
             selectMediaDevice.value = 'None';
         }
@@ -167,34 +155,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // Event listener for when a file is selected
         input.addEventListener('change', function(event) {
-            const file = event.target.files[0]; // Get the selected file
+            const file = event.target.files[0];
             if (file) {
                 displaySelectedImage(file);
             }
         });
 
-        // Trigger a click event on the input element to open the file explorer
+        // Trigger a click event to open the file explorer
         input.click();
     });
 
-    // Event listener for the "Save" button
     btnSave.addEventListener("click", async function() {
         try {
             checkSelectValue()
-            // Open IndexedDB database
             const db = await idb.openDB('photos', 1);
     
-            // Get the object store
             const tx = db.transaction('photos', 'readonly');
             const store = tx.objectStore('photos');
     
-            // Get all keys (IDs) from the object store
             const keys = await store.getAllKeys();
     
-            // Find the maximum key value (ID)
             const maxKey = Math.max(...keys);
     
-            // Check if a maximum key is found
             if (!isNaN(maxKey)) {
                 saveText(maxKey);
             } else {
@@ -205,25 +187,18 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Event listener for the "Restart" button
     btnRestart.addEventListener("click", async function() {
         try {
-            // Open IndexedDB database
             const db = await idb.openDB('photos', 1);
     
-            // Get all keys in the object store
             const keys = await db.getAllKeys('photos');
     
-            // Get the ID of the last item
             const lastItemId = keys[keys.length - 1];
     
-            // If there are items in the database
             if (lastItemId !== undefined) {
-                // Remove the last item from the object store
                 await db.delete('photos', lastItemId);
                 console.log('Last item removed from IndexedDB.');
     
-                // Show the camera and hide the saved photo
                 videoElement.style.display = 'block';
                 photoElement.innerHTML = '';
                 photoElement.classList.add('hidden');
@@ -237,8 +212,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
     
-
-    // Function to take and save photo
     async function takeAndSavePhoto() {
         try {
             if (!_mediaStream) {
@@ -253,13 +226,10 @@ document.addEventListener('DOMContentLoaded', function() {
             canvas.height = video.videoHeight;
             context.drawImage(video, 0, 0, canvas.width, canvas.height);
     
-            // Get data URL of the captured image
             const dataUrl = canvas.toDataURL('image/jpeg');
     
-            // Convert data URL to Blob
             const blob = await fetch(dataUrl).then(res => res.blob());
     
-            // Open IndexedDB database
             const db = await idb.openDB('photos', 1, {
                 upgrade(db) {
                     const photosStore = db.createObjectStore('photos', { autoIncrement: true });
@@ -268,17 +238,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 },
             });
     
-            // Save the photo to IndexedDB
             const tx = db.transaction('photos', 'readwrite');
             const store = tx.objectStore('photos');
-            const photoId = await store.add({ blob, text: '' }); // Add an empty text field
+            const photoId = await store.add({ blob, text: '' }); // Add no text to text field
             await tx.done;
     
             console.log("Photo saved with ID:", photoId);
     
-            // Show saved photo
             await showSavedPhoto(photoId);
-            // Extract text from the photo
             await extractTextFromPhoto(photoId);
         } catch (error) {
             console.error('Error taking and saving photo:', error);
@@ -287,40 +254,31 @@ document.addEventListener('DOMContentLoaded', function() {
 
     async function SavePhotoFromFile() {
         try {
-            // Get the selected image element
             const imgElement = document.querySelector('#photoElement img');
             if (!imgElement) {
                 console.error("No image found in photoElement.");
                 return;
             }
     
-            // Create a canvas to draw the image
             const canvas = document.createElement('canvas');
             const context = canvas.getContext('2d');
     
-            // Set canvas dimensions to match image dimensions
             canvas.width = imgElement.naturalWidth || imgElement.width;
             canvas.height = imgElement.naturalHeight || imgElement.height;
     
-            // Draw the image onto the canvas
             context.drawImage(imgElement, 0, 0, canvas.width, canvas.height);
     
-            // Get data URL of the captured image
             const dataUrl = canvas.toDataURL('image/jpeg');
     
-            // Convert data URL to Blob
             const blob = await fetch(dataUrl).then(res => res.blob());
     
-            // Open IndexedDB database
             const db = await idb.openDB('photos', 1, {
                 upgrade(db) {
                     const photosStore = db.createObjectStore('photos', { autoIncrement: true });
-                    // Add a text field to the object store
                     photosStore.createIndex('text', 'text', { unique: false });
                 },
             });
     
-            // Save the photo to IndexedDB
             const tx = db.transaction('photos', 'readwrite');
             const store = tx.objectStore('photos');
             const photoId = await store.add({ blob, text: '' }); // Add an empty text field
@@ -328,23 +286,17 @@ document.addEventListener('DOMContentLoaded', function() {
     
             console.log("Photo saved with ID:", photoId);
     
-            // Show saved photo
             await showSavedPhoto(photoId);
-            // Extract text from the photo
             await extractTextFromPhoto(photoId);
         } catch (error) {
             console.error('Error taking and saving photo from photoElement:', error);
         }
     }
     
-
-    // Function to display saved photo
     async function showSavedPhoto(photoId) {
         try {
-            // Open IndexedDB database
             const db = await idb.openDB('photos', 1);
     
-            // Get the photo blob from IndexedDB
             const tx = db.transaction('photos', 'readonly');
             const store = tx.objectStore('photos');
             const photoData = await store.get(photoId);
@@ -353,28 +305,22 @@ document.addEventListener('DOMContentLoaded', function() {
                 const blob = photoData.blob;
                 const dataUrl = URL.createObjectURL(blob);
     
-                // Create image element
                 const img = document.createElement('img');
                 img.src = dataUrl;
                 img.alt = 'Saved Photo';
                 img.style.maxWidth = '100%';
                 
-                // Hide videoElement
                 if (videoElement) {
                     videoElement.style.display = 'none';
     
-                    // Remove embed-responsive class from parent div
                     const parentDiv = videoElement.parentElement;
                     parentDiv.classList.remove('embed-responsive', 'embed-responsive-16by9');
-                    // Add a custom class for maintaining aspect ratio
                     parentDiv.classList.add('custom-aspect-ratio');
                 }
 
-                // Display the photo in the div
                 photoElement.innerHTML = ''; // Clear previous content
                 photoElement.appendChild(img);
     
-                // Revoke the object URL after the image is loaded or no longer needed
                 img.addEventListener('load', () => {
                     URL.revokeObjectURL(dataUrl);
                 });
@@ -386,13 +332,10 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
     
-    // Function to extract text from photo stored in IndexedDB
     async function extractTextFromPhoto(photoId) {
         try {
-            // Open IndexedDB database
             const db = await idb.openDB('photos', 1);
 
-            // Get the photo blob from IndexedDB
             const tx = db.transaction('photos', 'readonly');
             const store = tx.objectStore('photos');
             const photoData = await store.get(photoId);
@@ -406,7 +349,6 @@ document.addEventListener('DOMContentLoaded', function() {
             // Use Tesseract.js to recognize text from the image blob
             const { data: { text } } = await Tesseract.recognize(blob);
 
-            // Display the extracted text in the textarea
             textareaOCR.value = text;
             textareaOCR.classList.remove('hidden');
 
@@ -416,38 +358,31 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    // Change the height of the textarea based on content
     function adjustTextareaHeight() {
         const lines = textareaOCR.value.split('\n').length;
         const minRows = textareaOCR.getAttribute('rows');
+
         // Calculate the number of rows based on content height
         let rows = Math.max(minRows, lines);
-        // Set the height of the textarea
         textareaOCR.rows = rows;
     }
     
-    // Function to display the selected image
     function displaySelectedImage(file) {
         try {
-            // Create URL for the selected image
             const imageUrl = URL.createObjectURL(file);
 
-            // Create image element
             const img = document.createElement('img');
             img.src = imageUrl;
             img.alt = 'Selected Image';
             img.style.maxWidth = '100%';
 
-            // Hide videoElement if present
             if (videoElement) {
                 videoElement.style.display = 'none';
             }
 
-            // Display the selected image in the photoElement div
             photoElement.innerHTML = ''; // Clear previous content
             photoElement.appendChild(img);
 
-            // Revoke the object URL after the image is loaded or no longer needed
             img.addEventListener('load', () => {
                 URL.revokeObjectURL(imageUrl);
             });
@@ -456,28 +391,21 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
     
-    // Function to save text to IndexedDB
     async function saveText(photoId) {
         console.log("Photo ID: ", photoId);
         try {
-            // Get the text from the textarea
             const text = textareaOCR.value.trim(); // Trim any leading or trailing whitespace
 
-            // Open IndexedDB database
             const db = await idb.openDB('photos', 1);
 
-            // Get the object store
             const tx = db.transaction('photos', 'readwrite');
             const store = tx.objectStore('photos');
 
-            // Get the photo data object by photoId
             const photoData = await store.get(photoId);
 
             if (photoData) {
-                // Update the text field in the photo data object
                 photoData.text = text;
 
-                // Put the updated photo data object back into the object store
                 await store.put(photoData, photoId);
 
                 console.log('Text saved successfully:', text);
@@ -505,25 +433,18 @@ function checkSelectValue() {
 
 async function checkPrevDBItem() {
     try {
-        // Open IndexedDB database
         const db = await idb.openDB('photos', 1);
 
-        // Get all keys in the object store
         const keys = await db.getAllKeys('photos');
 
-        // Get the ID of the last item
         const lastItemId = keys[keys.length - 1];
 
-        // If there are items in the database
         if (lastItemId !== undefined) {
-            // Get the last item from the object store
             const lastItem = await db.get('photos', lastItemId);
 
-            // Check if the last item contains text
             if (lastItem.text.trim() !== '') {
                 console.log('Last item contains text. Skipping deletion.');
             } else {
-                // Delete the last item from the object store
                 await db.delete('photos', lastItemId);
                 console.log('Last item removed from IndexedDB.');
             }
@@ -538,25 +459,18 @@ async function checkPrevDBItem() {
 window.onload = async function() {
     try {
         checkSelectValue();
-        // Open IndexedDB database
         const db = await idb.openDB('photos', 1);
 
-        // Get all keys in the object store
         const keys = await db.getAllKeys('photos');
 
-        // Get the ID of the last item
         const lastItemId = keys[keys.length - 1];
 
-        // If there are items in the database
         if (lastItemId !== undefined) {
-            // Get the last item from the object store
             const lastItem = await db.get('photos', lastItemId);
 
-            // Check if the last item contains text
             if (lastItem.text.trim() !== '') {
                 console.log('Last item contains text. Skipping deletion.');
             } else {
-                // Delete the last item from the object store
                 await db.delete('photos', lastItemId);
                 console.log('Last item removed from IndexedDB.');
             }
